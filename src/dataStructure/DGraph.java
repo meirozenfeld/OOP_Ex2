8232bd50eefb5abef2247036b9e153c29ff1e4ef
meirozenfeld <meirozenfeld@gmail.com>
2019-12-25 13:55:33 +0200

    adding Node, Edge clases

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -1,73 +1,107 @@
 package dataStructure;
 
 import java.util.Collection;
+import java.util.Hashtable;
 
 public class DGraph implements graph{
+	private Hashtable  <Integer, node_data> Nodes;
+	private Hashtable <node_data,Hashtable<Integer, edge_data>> Edges; // hashtable into hashtable with Node key (and dest key)
+	private int edgeSize;
+	private int mc=0; // mode count
 
+	public DGraph() {
+		 Nodes=new Hashtable <Integer, node_data>();
+		 Edges=new Hashtable<node_data,Hashtable<Integer, edge_data>>();
+		edgeSize=0;
+		this.mc=0;
+	}
 	@Override
 	public node_data getNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		return Nodes.get(key);
 	}
 
 	@Override
 	public edge_data getEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		if(Nodes.get(src)==null||Nodes.get(dest)==null||src==dest)return null;
+		node_data v=Nodes.get(src);
+		return Edges.get(v).get(dest);
 	}
 
 	@Override
 	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
-		
+		int k=n.getKey();
+		this.Nodes.put(k ,n);
+		this.Edges.put(n, new Hashtable<Integer,edge_data>());
+		this.mc++;
 	}
 
 	@Override
 	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
-		
+		edge_data e=new EdgeData(src,dest,w);	
+		Edges.get(Nodes.get(src)).put(dest, e);
+		edgeSize++;
+		this.mc++;
 	}
+
 
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return this.Nodes.values();
 	}
 
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		return Edges.get(Nodes.get(node_id)).values();
 	}
 
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
-		return null;
+		node_data temp=Nodes.get(key);
+		if(temp==null)
+		{
+			return temp;
+		}
+		this.Nodes.remove(key);
+		this.mc++;
+		return temp;
 	}
 
 	@Override
 	public edge_data removeEdge(int src, int dest) {
-		// TODO Auto-generated method stub
-		return null;
+		edge_data temp=Edges.get(Nodes.get(src)).get(dest);
+		if(temp==null)
+		{
+			return temp;
+		}
+		Edges.get(Nodes.get(src)).remove(dest);
+		this.mc++;
+		return temp;
 	}
 
 	@Override
 	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Nodes.size();
 	}
 
 	@Override
 	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.edgeSize;
 	}
 
 	@Override
 	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.mc;
+	}
+	
+	public String tostring () {
+		String ans="";
+
+		for(Integer key: this.Nodes.keySet()){
+			ans+="            "+key+": "+ this.Edges.get(this.Nodes.get(key)).toString();
+			
+		}
+		return ans;
 	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,3 @@
 module OOP_Ex2 {
+	requires java.desktop;
 }
